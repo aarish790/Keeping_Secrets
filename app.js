69ece70cf58fff34357c3c35ddd2cbce54360611
aarish790 <60058807+aarish790@users.noreply.github.com>
2020-06-29 17:12:09 +0530

    last

--- app.js
+++ app.js
@@ -175,12 +175,8 @@ app.post("/login",function(req,res){
 	
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
 
 
-app.listen(port, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server has started successfully");
 });
